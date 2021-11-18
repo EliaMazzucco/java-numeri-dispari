4d3d8d6a09e65f6c0b793a93c4d828c74a524121
elia.mazzucco95@gmail.com <93248840+EliaMazzucco@users.noreply.github.com>
2021-11-18 17:05:38 +0100

    risoluzione problema numeri dispari

--- src/org/generation/italy/cicli/StampaNumeriDispari.java
+++ src/org/generation/italy/cicli/StampaNumeriDispari.java
@@ -15,54 +15,30 @@ public class StampaNumeriDispari {
 		Scanner scanner = new Scanner (System.in) ;	
 		
 		int lunghezza = scanner.nextInt();	
-		System.out.println("Creo un array lungo " + lunghezza);	
+		System.out.println("Creo un array lungo " + lunghezza + " con numeri causali: ");	
 		
 		int[] n = new int[lunghezza];
 		
+		
 		for(int i=0; i<n.length; i++ ) {		
-		
-		
-		Random randomGenerator = new Random();
-		int max = 100;
-		int numeriRandom = randomGenerator.nextInt(max);
-		
-		n[i] = numeriRandom;
-		
-		
-		System.out.println(n[i]);	
-		}
-
-
-		
-					
-		
+			
 		//Popolarlo con numeri interi casuali compresi tra 0 e 100 (0 incluso, 100 escluso)
 		//Scorrere gli elementi  dell’array e stampare a video soltanto i numeri dispari
 		
-
-		
-			
-
-			
-		
-		
-		
+		Random randomGenerator = new Random();
+		int max = 100;
+		int var = randomGenerator.nextInt(max);
+		if(var % 2 == 0) {
+			var++;			
+		}
 	
+		n[i] = var;		
+		}
+		
+		for(int j=0; j<n.length; j++) {	
 			
-
-		
-		
-		
-		
-		
-		
-		
-	
-
-		
-
-		
+				System.out.println(n[j]);			
+		}				
 		scanner.close();
-	}
-	
+	}	
 }
