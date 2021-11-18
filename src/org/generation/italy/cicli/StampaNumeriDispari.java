ea48c338c7a6203d96e59ef24af0fb8d6a3ed49a
elia.mazzucco95@gmail.com <93248840+EliaMazzucco@users.noreply.github.com>
2021-11-18 16:01:12 +0100

    popolato con numeri interi causali

--- src/org/generation/italy/cicli/StampaNumeriDispari.java
+++ src/org/generation/italy/cicli/StampaNumeriDispari.java
@@ -1,5 +1,6 @@
 package org.generation.italy.cicli;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class StampaNumeriDispari {
@@ -12,27 +13,52 @@ public class StampaNumeriDispari {
 		
 		System.out.println("stabilisci la lunghezza: ");
 		Scanner scanner = new Scanner (System.in) ;	
-		int lunghezza = scanner.nextInt();
 		
-		//Popolarlo con numeri interi casuali compresi tra 0 e 100 (0 incluso, 100 escluso)
-		
-		if(lunghezza>=0 && lunghezza < 100) {
-		System.out.println("Creo un array lungo " + lunghezza);
-		
-		//Scorrere gli elementi  dell’array e stampare a video soltanto i numeri dispari
+		int lunghezza = scanner.nextInt();	
+		System.out.println("Creo un array lungo " + lunghezza);	
 		
 		int[] n = new int[lunghezza];
 		
-		for(int i = 0; i < n.length ; i++) {
-			n[i] += i+1;
-			System.out.println(n[i] + " ");
-		}		
+		for(int i=0; i<n.length; i++ ) {		
 		
 		
+		Random randomGenerator = new Random();
+		int max = 100;
+		int numeriRandom = randomGenerator.nextInt(max);
 		
-		}else {
-			System.out.println("Inserisci un numero tra 0 e 99");
+		n[i] = numeriRandom;
+		
+		
+		System.out.println(n[i]);	
 		}
+
+
+		
+					
+		
+		//Popolarlo con numeri interi casuali compresi tra 0 e 100 (0 incluso, 100 escluso)
+		//Scorrere gli elementi  dell’array e stampare a video soltanto i numeri dispari
+		
+
+		
+			
+
+			
+		
+		
+		
+	
+			
+
+		
+		
+		
+		
+		
+		
+		
+	
+
 		
 
 		
